668b13c16d3bcb7e014f0c81237283d1811589fe
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-12-30 22:23:04 +0000

    horizontal search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+    if (l.includes(word)) { 
+      return true;
     }
+  }
+  return false;
 }
+
+
+
+const transpose = function(matrix) {
+    let muse = [];
+    for (let col = 0; col < matrix[0].length; col++) {
+      muse.push(matrix.map(x => x[col]))
+      // console.log(muse)
+    }
+    main = muse;
+  
+    return main;
+};
+
+
+
+
 
 module.exports = wordSearch
